5ee95c70a76ad8690a7c8d9cac02969f2544d735
Manny <mbarre13@lion.lmu.edu>
2020-11-24 13:20:11 -0800

    switched to bidirectional + 2 layer lstm, and fixed inconsistent tensor devices

--- lstm.py
+++ lstm.py
@@ -2,8 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 from data_loader import IX_TO_LABEL, LABEL_TO_IX, load_data
 
@@ -13,13 +13,14 @@ class LSTM(nn.Module):
     Implementation of LSTM neural network. Set the init parameter `bidirectional` to True 
     to use a bidirectional LSTM (a BLSTM).
     """
+
     def __init__(self, vocab_size, hidden_size, output_size, num_layers, bidirectional=False):
         super().__init__()
         self.bidirectional = bidirectional
         self.emb = nn.Embedding(vocab_size, hidden_size)
         self.rec = nn.LSTM(hidden_size, hidden_size,
                            num_layers=num_layers, bidirectional=bidirectional)
-        # When bidirectional, the output of the LSTM will be twice as long, so we need to 
+        # When bidirectional, the output of the LSTM will be twice as long, so we need to
         # multiply the next layer's dims by 2
         linear_size = hidden_size if not bidirectional else hidden_size * 2
         self.lin = nn.Linear(linear_size, output_size)
@@ -27,13 +28,14 @@ class LSTM(nn.Module):
 
     def forward(self, input_seq):
         embeds = self.emb(input_seq)
-        output_seq, (h_last, c_last) = self.rec(embeds.view(len(input_seq), 1, -1))
+        output_seq, (h_last, c_last) = self.rec(
+            embeds.view(len(input_seq), 1, -1))
 
         h = None
-        
+
         if self.bidirectional:
-            h_direc_1 = h_last[0, :, :]
-            h_direc_2 = h_last[1, :, :]
+            h_direc_1 = h_last[1, :, :]
+            h_direc_2 = h_last[2, :, :]
             h = torch.cat((h_direc_1, h_direc_2), dim=1)
         else:
             h = h_last[0, :, :]
@@ -65,11 +67,12 @@ def strings_to_tensors(posts_array, tok_to_ix):
     tensors = []
     for post in posts_array:
         tokens = post.split(' ')
-        x = [tok_to_ix[tok] if tok in tok_to_ix else tok_to_ix['<UNK>'] for tok in tokens]
+        x = [tok_to_ix[tok] if tok in tok_to_ix else tok_to_ix['<UNK>']
+             for tok in tokens]
         x_train_tensor = torch.LongTensor(x).to(device)
         tensors.append(x_train_tensor)
     return tensors
-    
+
 
 if __name__ == "__main__":
 
@@ -79,7 +82,6 @@ if __name__ == "__main__":
     """
     Load the training, validation, and test data
     """
-
     posts, labels = load_data('data/reddit_submissions.csv')
 
     posts_train, posts_test, train_labels, test_labels = train_test_split(
@@ -95,16 +97,15 @@ if __name__ == "__main__":
     """
     Specify model's hyperparameters and architecture
     """
-
-    hidden_size = 3
+    hidden_size = 25
     learning_rate = 0.01
-    num_layers = 1
+    num_layers = 2
     vocab_size = len(tok_to_ix)
     output_size = len(np.unique(train_labels))
     n_epochs = 3
 
     model = LSTM(vocab_size=vocab_size,
-                hidden_size=hidden_size, output_size=output_size, num_layers=num_layers)
+                 hidden_size=hidden_size, output_size=output_size, num_layers=num_layers, bidirectional=True)
     model = model.to(device)
     loss_func = nn.BCELoss()
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
@@ -116,7 +117,8 @@ if __name__ == "__main__":
     for epoch in range(1, n_epochs + 1):
         model.train()
 
-        print(f"""\n--------------\nBeginning Epoch {epoch} of {n_epochs}\n--------------\n""")
+        print(
+            f"""\n--------------\nBeginning Epoch {epoch} of {n_epochs}\n--------------\n""")
 
         # Accumulate loss for all samples in this epoch
         running_loss = 0
@@ -126,19 +128,20 @@ if __name__ == "__main__":
 
         # Gradient descent algorithm for each data sample
         for x_train_tensor, correct_label in zip(train_data, train_labels):
-            
+
             sample_counter += 1
             if sample_counter % 1000 == 0:
-                print("  > sample {} of {}".format(sample_counter, num_samples))
+                print("  > sample {} of {}".format(
+                    sample_counter, num_samples))
 
             # Make a prediction on the training data
             y_predicted_tensor = model(x_train_tensor)
 
-            # Create the true label's tensor by creating a zeros-array with the element at the 
+            # Create the true label's tensor by creating a zeros-array with the element at the
             # correct label's index set to 1.0.
             y_true_list = np.zeros(output_size)
             y_true_list[correct_label] = 1.0
-            y_true_tensor = torch.Tensor([y_true_list])
+            y_true_tensor = torch.Tensor([y_true_list]).to(device)
 
             # Backpropagation
             loss = loss_func(y_predicted_tensor, y_true_tensor)
@@ -150,7 +153,8 @@ if __name__ == "__main__":
             with torch.no_grad():
                 running_loss += loss.item()
 
-        print(f"""\n--------------\nEnd of Epoch {epoch}\nLoss: {running_loss/len(train_labels)}\n--------------\n""")
+        print(
+            f"""\n--------------\nEnd of Epoch {epoch}\nLoss: {running_loss/sample_counter}\n--------------\n""")
 
     """ 
     Evaluate the model
@@ -160,6 +164,7 @@ if __name__ == "__main__":
         predicted_labels = []
         for x_test_tensor, correct_label in zip(test_data, test_labels):
             y_predicted_tensor = model(x_test_tensor)
+            y_predicted_tensor = y_predicted_tensor.to('cpu')
 
             # Store the labels that the model predicts so that we can calculate the accuracy, etc. later
             predicted_label = np.argmax(y_predicted_tensor.data.numpy())
@@ -167,7 +172,8 @@ if __name__ == "__main__":
 
         print("\n==============================")
         print("\nEvaluation")
-        report = classification_report(y_true=test_labels, y_pred=predicted_labels)
+        report = classification_report(
+            y_true=test_labels, y_pred=predicted_labels)
         print(report)
         print("\n==============================")
 
@@ -177,19 +183,16 @@ Output:
 
 ==============================
 
-Evaluation
-/Users/lucillenjoo/Documents/537/dv-classifier/env/lib/python3.8/site-packages/sklearn/metrics/_classification.py:1221: UndefinedMetricWarning: Precision and F-score are ill-defined and being set to 0.0 in labels with no predicted samples. Use `zero_division` parameter to control this behavior.
-  _warn_prf(average, modifier, msg_start, len(result))
+Evaluation:
               precision    recall  f1-score   support
 
-           0       0.00      0.00      0.00       251
-           1       0.00      0.00      0.00       327
-           2       0.92      1.00      0.96      6464
+           0       0.00      0.00      0.00       253
+           1       0.66      0.71      0.69       324
+           2       0.96      0.99      0.97      6465
 
-    accuracy                           0.92      7042
-   macro avg       0.31      0.33      0.32      7042
-weighted avg       0.84      0.92      0.88      7042
-
+    accuracy                           0.94      7042
+   macro avg       0.54      0.57      0.55      7042
+weighted avg       0.91      0.94      0.93      7042
 
 ==============================
 """
