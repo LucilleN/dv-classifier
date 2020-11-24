8a9608cc2afb72f11dd73accc2261fdafe6e424a
Lucille Njoo <lucillenjoo@gmail.com>
2020-11-24 02:39:00 -0800

    Trying without bidirectional; reorganizing and cleaning things up

--- lstm.py
+++ lstm.py
@@ -1,50 +1,51 @@
-import random
-import time
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from progressbar import progressbar
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
 from data_loader import IX_TO_LABEL, LABEL_TO_IX, load_data
 
-# If there's an available GPU, lets train on it
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
-
-posts, labels = load_data('data/reddit_submissions.csv')
-train_data, test_data, train_label, test_label = train_test_split(
-    posts, labels, test_size=0.2)
-
 
 class LSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_size, output_size, num_layers):
+    """
+    Implementation of LSTM neural network. Set the init parameter `bidirectional` to True 
+    to use a bidirectional LSTM (a BLSTM).
+    """
+    def __init__(self, vocab_size, hidden_size, output_size, num_layers, bidirectional=False):
         super().__init__()
+        self.bidirectional = bidirectional
         self.emb = nn.Embedding(vocab_size, hidden_size)
         self.rec = nn.LSTM(hidden_size, hidden_size,
-                           num_layers=num_layers, bidirectional=True)
-
-        # (hidden_size * 2) since bidirectional
-        self.lin = nn.Linear(hidden_size * 2, output_size)
+                           num_layers=num_layers, bidirectional=bidirectional)
+        # When bidirectional, the output of the LSTM will be twice as long, so we need to 
+        # multiply the next layer's dims by 2
+        linear_size = hidden_size if not bidirectional else hidden_size * 2
+        self.lin = nn.Linear(linear_size, output_size)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_seq):
-        # embeds = self.emb(input_seq)
         embeds = self.emb(input_seq)
         output_seq, (h_last, c_last) = self.rec(embeds.view(len(input_seq), 1, -1))
 
-        h_direc_1 = h_last[0, :, :]
-        h_direc_2 = h_last[1, :, :]
-        h = torch.cat((h_direc_1, h_direc_2), dim=1)
+        h = None
+        
+        if self.bidirectional:
+            h_direc_1 = h_last[0, :, :]
+            h_direc_2 = h_last[1, :, :]
+            h = torch.cat((h_direc_1, h_direc_2), dim=1)
+        else:
+            h = h_last[0, :, :]
 
         scores = self.lin(h)
-        # return scores
         return self.sigmoid(scores)
 
+
 def build_vocab(posts):
+    """
+    Given the training set of posts, constructs the vocabulary dictionary, `tok_to_ix`, that
+    maps unique tokens to their index in the vocabulary.
+    """
     tok_to_ix = {}
     for post in posts:
         tokens = post.split(' ')
@@ -52,72 +53,127 @@ def build_vocab(posts):
             tok_to_ix.setdefault(token, len(tok_to_ix))
     # Manually add our own placeholder tokens
     tok_to_ix.setdefault('<UNK>', len(tok_to_ix))
-    tok_to_ix.setdefault('<s>', len(tok_to_ix))
-    tok_to_ix.setdefault('<e>', len(tok_to_ix))
     return tok_to_ix
 
-# vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=0)
-# tok_to_ix = vectorizer.vocabulary_
-# train_data = vectorizer.fit_transform(train_data)
-tok_to_ix = build_vocab(train_data)
 
-# Hyperparamaters
-hidden_size = 3
-learning_rate = 0.01
-num_layers = 1
-# vocab_size = len(vectorizer.vocabulary_)
-vocab_size = len(tok_to_ix)
-output_size = len(np.unique(train_label))
-n_epochs = 3
-
-model = LSTM(vocab_size=vocab_size,
-             hidden_size=hidden_size, output_size=output_size, num_layers=num_layers)
-# model = model.to(device)
-# criterion = nn.NLLLoss()
-criterion = nn.BCELoss()
-# criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-for epoch in range(n_epochs):
-    model.train()
-
-    print("EPOCH {} OF {}".format(epoch, n_epochs))
-    # Training performance tracking
-    running_loss = 0
-
-    counter = 0
-    # for post, correct_label in progressbar(zip(train_data, train_label)):
-    for post, correct_label in zip(train_data, train_label):
-        counter += 1
-        if counter % 200 == 0:
-            print("sample {}".format(counter))
+def strings_to_tensors(posts_array, tok_to_ix):
+    """
+    Converts each string in an array of strings into a tensor that we will input into the model
+    so that we don't have to convert each sample to a tensor again at each epoch.
+    """
+    tensors = []
+    for post in posts_array:
         tokens = post.split(' ')
-        x = [tok_to_ix[tok] for tok in tokens if tok in tok_to_ix ]
-        x_train_tensor = torch.LongTensor(x) #.to(device)
+        x = [tok_to_ix[tok] if tok in tok_to_ix else tok_to_ix['<UNK>'] for tok in tokens]
+        x_train_tensor = torch.LongTensor(x).to(device)
+        tensors.append(x_train_tensor)
+    return tensors
+    
 
-        # Feed net training data
-        y_predicted_tensor = model(x_train_tensor)
-        # print("predicted_scores: {}".format(y_predicted_tensor))
+if __name__ == "__main__":
 
-        y_true_list = np.zeros(output_size)
-        y_true_list[correct_label] = 1.0
-        y_true_tensor = torch.Tensor([y_true_list])
-        # print("y_true_tensor: {}".format(y_true_tensor))
+    # If there's an available GPU, lets train on it
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        # Backprop
-        loss = criterion(y_predicted_tensor, y_true_tensor)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+    """
+    Load the training, validation, and test data
+    """
 
-        # Compute metrics
-        with torch.no_grad():
-            running_loss += loss.item()
+    posts, labels = load_data('data/reddit_submissions.csv')
 
-    print('t')
-    print(f"""
-    --------------
-    Epoch #{epoch}
-    Loss: {running_loss/len(train_label)}%
-    --------------\n
-    """)
+    posts_train, posts_test, train_label, test_label = train_test_split(
+        posts, labels, test_size=0.2)
+
+    tok_to_ix = build_vocab(posts_train)
+
+    # Convert all posts to tensors that we will input into the model so that we do
+    # not have to convert them again at every epoch
+    train_data = strings_to_tensors(posts_train, tok_to_ix)
+    test_data = strings_to_tensors(posts_test, tok_to_ix)
+
+    """
+    Specify model's hyperparameters and architecture
+    """
+
+    hidden_size = 3
+    learning_rate = 0.01
+    num_layers = 1
+    vocab_size = len(tok_to_ix)
+    output_size = len(np.unique(train_label))
+    n_epochs = 3
+
+    model = LSTM(vocab_size=vocab_size,
+                hidden_size=hidden_size, output_size=output_size, num_layers=num_layers)
+    model = model.to(device)
+    loss_func = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    """
+    Train the model
+    """
+
+    for epoch in range(1, n_epochs + 1):
+        model.train()
+
+        print(f"""\n--------------\nBeginning Epoch {epoch} of {n_epochs}\n--------------\n""")
+
+        # Accumulate loss for all samples in this epoch
+        running_loss = 0
+
+        num_samples = len(train_data)
+        sample_counter = 0
+
+        # Gradient descent algorithm for each data sample
+        for x_train_tensor, correct_label in zip(train_data, train_label):
+            
+            sample_counter += 1
+            if sample_counter % 1000 == 0:
+                print("  > sample {} of {}".format(sample_counter, num_samples))
+            
+            # tokens = post.split(' ')
+            # x = [tok_to_ix[tok] if tok in tok_to_ix else tok_to_ix['<UNK>'] for tok in tokens]
+            # x_train_tensor = torch.LongTensor(x).to(device)
+
+            # Make a prediction on the training data
+            y_predicted_tensor = model(x_train_tensor)
+
+            # Create the true label's tensor by creating a zeros-array with the element at the 
+            # correct label's index set to 1.0.
+            y_true_list = np.zeros(output_size)
+            y_true_list[correct_label] = 1.0
+            y_true_tensor = torch.Tensor([y_true_list])
+
+            # Backpropagation
+            loss = loss_func(y_predicted_tensor, y_true_tensor)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Compute metrics
+            with torch.no_grad():
+                running_loss += loss.item()
+
+        print(f"""\n--------------\nEnd of Epoch {epoch}\nLoss: {running_loss/len(train_label)}%\n--------------\n""")
+
+    """ 
+    Evaluate the model
+    """
+    with torch.no_grad():
+        model.eval()
+        predicted_labels = []
+        for post, correct_label in zip(train_data, train_label):
+            pred_y_tensor = make_single_prediction(model=model, text=tweet, tok_to_ix=tok_to_ix)
+
+            predicted_label = np.argmax(pred_y_tensor.data.numpy())
+            predicted_labels.append(predicted_label)
+
+        print("\n==============================")
+        print("\nEvaluation")
+
+        accuracy = get_accuracy(labels, predicted_labels)
+        print("Accuracy:", accuracy)
+
+        print("\n==============================\n")
+
+        report = classification_report(y_true=labels, y_pred=predicted_labels)
+        print(report)
