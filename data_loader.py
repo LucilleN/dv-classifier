06b28e729e4c16cbc7dbdf3e66fe36e63741cb1d
coopslarhette <coopslarhette@gmail.com>
2020-12-02 16:34:31 -0800

    adds nlpaug stuff

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,11 @@
 import csv
 import numpy as np
+import random
+
+import nlpaug.augmenter.char as nac
+import nlpaug.augmenter.word as naw
+import nlpaug.augmenter.sentence as nas
+import nlpaug.flow as nafc
 
 # The domesticviolence and survivorsofabuse subreddits will be class 0, critical; these are personal stories, calls for help, requests for advice
 # The abuseInterrupted subreddit will be class 1, noncritical; it mostly contains empty text, links to articles, general statements about abuse, etc.
@@ -50,3 +56,21 @@ def load_data(file_path):
             posts.append(post_title_and_text)
 
     return (np.array(posts), np.array(labels))
+
+
+def augment_data():
+    aug = naw.WordEmbsAug(
+        model_type='word2vec', model_path='./GoogleNews-vectors-negative300.bin',
+        action="substitute")
+    with open('data/reddit_submissions.csv') as f:
+        reader = csv.reader(f)
+        # Skip the first row that just has column names
+        rows = list(reader)[1:]
+        for i in range(3):
+            text = random.choice(rows)[3]
+            print(text)
+            augmented = aug.augment(text)
+            print(augmented)
+
+
+augment_data()
