2b077a22152addf3be5f84d4c053782356e62a25
Lucille Njoo <lucillenjoo@gmail.com>
2020-12-06 15:58:37 -0800

    Update data loader function call so it uses augmented data and only a fraction of class 2

--- baseline_lstm.py
+++ baseline_lstm.py
@@ -53,7 +53,12 @@ if __name__ == "__main__":
     """
     Load the training, validation, and test data
     """
-    posts, labels = load_data('data/reddit_submissions.csv')
+    posts, labels = load_data(
+        og_file_path='data/reddit_submissions.csv', 
+        aug_file_path='data/augmented_reddit_submissions.csv', 
+        include_og=True, 
+        include_aug=True,
+        fraction_class_2_to_load=0.1)
 
     posts_train, posts_test, train_labels, test_labels = train_test_split(
         posts, labels, test_size=0.2)
