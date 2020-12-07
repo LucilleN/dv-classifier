89e38ab90602076b9faa5723a5c54c45534b3f8e
Manuel Barreto <30633532+mannybarreto@users.noreply.github.com>
2020-12-07 10:35:37 -0800

    adds data augmentation functions (#8)
    * Moved build_vocab and string_to_tensors to utils, added minibatch functions, and fixed device mismatch in rnn
    
    * implemented lstm minibatches
    
    * removed unused imports and formatted lstm
    
    * fixed lstm training loop
    
    * batched rnn training/eval
    
    * adds nlpaug stuff
    
    * adds bert aug
    
    * updates requirements.txt for transformers (we need 3.5.1, 4.0.0 is latest)
    
    * adds data aug read write code
    
    * Add argparse to data loader
    
    * refactors data augmenter to use parameters affecting num of new samples created by class and refactors for loop into new method
    
    * Implement data loading function parameters
    
    * adds new augmented reddit data
    
    * adds new augmented reddit data
    
    * adds synonym augmented data, renames bert augmented data
    
    * Reimplemented the reduction of class 2 samples so it's random rather than sequential
    
    * Update data loader function call so it uses augmented data and only a fraction of class 2
    
    * adds synonym data
    
    * Add nltk to requirements.txt
    
    Co-authored-by: coopslarhette <coopslarhette@gmail.com>
    Co-authored-by: Lucille Njoo <lucillenjoo@gmail.com>

--- data_loader.py
+++ data_loader.py
@@ -6,21 +6,32 @@ import nlpaug.augmenter.char as nac
 import nlpaug.augmenter.word as naw
 import nlpaug.augmenter.sentence as nas
 import nlpaug.flow as nafc
+import argparse
+
+# only need to run once for synonym generator
+import nltk
+nltk.download('wordnet')
 
 # The domesticviolence and survivorsofabuse subreddits will be class 0, critical; these are personal stories, calls for help, requests for advice
 # The abuseInterrupted subreddit will be class 1, noncritical; it mostly contains empty text, links to articles, general statements about abuse, etc.
 # Everything else will be class 2, general/unrelated
 
 CLASSES = {
-    'relationship_advice': 2, # 5874 samples
-    'relationships': 2, # 8201 samples
-    'casualconversation': 2, # 7286 samples
-    'advice': 2, # 5913 samples
-    'anxiety': 2, # 4183 samples
-    'anger': 2, # 837 samples
-    'abuseinterrupted': 1, # 1653 samples
-    'domesticviolence': 0, # 749 samples
-    'survivorsofabuse': 0 # 512 samples
+    'relationship_advice': 2,  # 5874 samples
+    'relationships': 2,  # 8201 samples
+    'casualconversation': 2,  # 7286 samples
+    'advice': 2,  # 5913 samples
+    'anxiety': 2,  # 4183 samples
+    'anger': 2,  # 837 samples
+    'abuseinterrupted': 1,  # 1653 samples
+    'domesticviolence': 0,  # 749 samples
+    'survivorsofabuse': 0  # 512 samples
+}
+
+CLASS_COUNTS = {
+    2: 32294,
+    1: 1653,
+    0: 1261
 }
 
 LABEL_TO_IX = {
@@ -35,49 +46,156 @@ IX_TO_LABEL = {
     2: 'general'
 }
 
-def load_data(file_path):
+
+def load_data(og_file_path, aug_file_path=None, include_og=True, include_aug=False, fraction_class_2_to_load=1.0):
     """
     Loads text and labels from dataset stored in file_path, a CSV.
     """
     posts = []
     labels = []
 
-    with open(file_path) as f:
-        reader = csv.reader(f)
-        # Skip the first row that just has column names
-        rows = list(reader)[1:]
-        for row in rows:
-            # print("\n" + str(row))
-            subreddit_name = row[0]
-            label = CLASSES[subreddit_name]
-            post_title = row[2]
-            post_text = row[3]
-            post_title_and_text = post_title + " " + post_text
-            labels.append(label)
-            posts.append(post_title_and_text)
+    sources = []
+    if include_og:
+        sources.append(og_file_path)
+    if include_aug:
+        sources.append(aug_file_path)
 
-    return (np.array(posts), np.array(labels))
+    class_2_max = int(fraction_class_2_to_load * CLASS_COUNTS[2])
+
+    for file_path in sources:
+        
+        with open(file_path) as f:
+            reader = csv.reader(f)
+
+            # Skip the first row that just has column names
+            rows = list(reader)[1:]
+            for row in rows:
+                # print("\n" + str(row))
+                subreddit_name = row[0]
+                label = CLASSES[subreddit_name]
+                post_title = row[2]
+                post_text = row[3]
+                post_title_and_text = post_title + " " + post_text
+                labels.append(label)
+                posts.append(post_title_and_text)
+
+    posts = np.array(posts)
+    labels = np.array(labels)
+
+    class_2_indexes = np.where(labels == 2)[0]
+    everything_else = np.where(labels != 2)[0]
+    class_2_subset_indexes = np.random.choice(class_2_indexes, size=class_2_max)
+    
+    class_2_posts = posts[class_2_subset_indexes]
+    class_2_labels = labels[class_2_subset_indexes]
+
+    all_other_posts = posts[everything_else]
+    all_other_labels = labels[everything_else]
+
+    posts = np.concatenate([all_other_posts, class_2_posts])
+    labels = np.concatenate([class_2_labels, all_other_labels])
+
+    return (posts, labels)
 
 
-def augment_data():
+def create_new_rows(seed_rows, num_new_rows, new_rows, aug):
+    # apparently faster to store these locally, be careful with maintenance though
+    augment = aug.augment
+    append = new_rows.append
+    for i in range(num_new_rows):
+        print(i)
+        row = random.choice(seed_rows)
+        # Augment the post title
+        row[2] = augment(row[2])
+        # Augment the post body
+        row[3] = augment(row[3])
+        append(row)
+
+
+def augment_data(num_new_class_0=1, num_new_class_1=1, clear_old_augmented_data=False):
+    # different models that can be used for augmenting:
     # aug = naw.WordEmbsAug(
-        # model_type='word2vec', model_path='./GoogleNews-vectors-negative300.bin',
-        # action="substitute")
-    # aug = naw.SynonymAug(aug_src='wordnet')
-    aug = naw.ContextualWordEmbsAug(
-        model_path='bert-base-uncased', action="insert")
+    # model_type='word2vec', model_path='./GoogleNews-vectors-negative300.bin',
+    # action="substitute")
+    # aug = naw.ContextualWordEmbsAug(
+    #     model_path='bert-base-uncased', action="insert", device='cpu')
+    
+    aug = naw.SynonymAug(aug_src='wordnet')
 
+    new_rows = []
     with open('data/reddit_submissions.csv') as f:
         reader = csv.reader(f)
         # Skip the first row that just has column names
         rows = list(reader)[1:]
-        for i in range(10):
-            text = random.choice(rows)[3]
-            print(text)
-            print('========================== END OF ORIGINAL TEXT =================================')
-            augamented = aug.augment(text)
-            print(augamented)
-            print('========================== END OF AUGMENTED TEXT =================================')
+        print('unfiltered rows: {}'.format(len(rows)))
+        seed_rows_with_class_0 = list(filter(lambda r: CLASSES[r[0]] == 0, rows))
+        seed_rows_with_class_1 = list(filter(lambda r: CLASSES[r[0]] == 1, rows))
+        print('filtered rows: {}'.format(len(seed_rows_with_class_0) + len(seed_rows_with_class_1)))
+        print('generating new data with class 0')
+        create_new_rows(seed_rows_with_class_0, num_new_class_0, new_rows, aug)
+        print('generating new data with class 1')
+        create_new_rows(seed_rows_with_class_1, num_new_class_1, new_rows, aug)
+
+    with open('data/augmented_reddit_submissions.csv', 'a') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
+        print('writing new rows')
+        writer.writerows(new_rows)
 
 
-augment_data()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--generate_augmented_data',
+                        action='store_true', help='If set, then trains network')
+    parser.add_argument('--clear_old_augmented_data',
+                        action='store_true', help='If set, then trains network')
+    parser.add_argument('--num_new_class_0',
+                        type=int, default=1000, help='Base learning rate (alpha)')
+    parser.add_argument('--num_new_class_1',
+                        type=int, default=1000, help='Base learning rate (alpha)')
+
+    args = parser.parse_args()
+
+    if args.generate_augmented_data:
+        augment_data(
+            num_new_class_0=args.num_new_class_0,
+            num_new_class_1=args.num_new_class_1,
+            clear_old_augmented_data=args.clear_old_augmented_data)
+
+    else: 
+        "Testing data loader"
+        posts, labels = load_data(og_file_path='data/reddit_submissions.csv')
+        print('just og data:', len(posts), len(labels))
+        class0 = labels[np.where(labels == 0)]
+        print('just og data class 0:', len(class0))
+        class1 = labels[np.where(labels == 1)]
+        print('just og data class 1:', len(class1))
+        class2 = labels[np.where(labels == 2)]
+        print('just og data class 2:', len(class2))
+
+        posts, labels = load_data(
+            og_file_path='data/reddit_submissions.csv', 
+            aug_file_path='data/augmented_reddit_submissions.csv', 
+            include_og=True, 
+            include_aug=True)
+        print('og + aug data:', len(posts), len(labels))
+        class0 = labels[np.where(labels == 0)]
+        print('og + aug data class 0:', len(class0))
+        class1 = labels[np.where(labels == 1)]
+        print('og + aug data class 1:', len(class1))
+        class2 = labels[np.where(labels == 2)]
+        print('og + aug data class 2:', len(class2))
+
+        posts, labels = load_data(
+            og_file_path='data/reddit_submissions.csv', 
+            aug_file_path='data/augmented_reddit_submissions.csv', 
+            include_og=True, 
+            include_aug=True,
+            fraction_class_2_to_load=0.1)
+        print('og + aug data with less 2:', len(posts), len(labels))
+        class0 = labels[np.where(labels == 0)]
+        print('og + aug data class 0 with less 2:', len(class0))
+        class1 = labels[np.where(labels == 1)]
+        print('og + aug data class 1 with less 2:', len(class1))
+        class2 = labels[np.where(labels == 2)]
+        print('og + aug data class 2 with less 2:', len(class2))
