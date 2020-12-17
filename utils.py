4b29fb51c3a5e09497368b6501b5a02484f736e2
Lucille Njoo <lucillenjoo@gmail.com>
2020-12-16 17:38:52 -0800

    Refactor training for lstm

--- utils.py
+++ utils.py
@@ -6,14 +6,28 @@ from argparse import ArgumentParser
 
 from data_loader import load_data
 
-parser = ArgumentParser(description='Neural DV-Classifier')
+def parse_command_line_args():
+    """
+    Handles the parsing of command line arguments for all files.
+    Returns `args`, the object that contains all the command line arguments,
+    which can be accessed as follows:
+        args.retrain
+        args.n_epochs
+        args.num_layers 
+    """
+    parser = ArgumentParser(description='Neural DV-Classifier')
 
-parser.add_argument('--train_from_scratch', action='store_true', dest='retrain', default=False)
-parser.add_argument('--use_og_data_only', action='store_true', default=False)
-parser.add_argument('--use_2_classes', action='store_true', default=False)
-parser.add_argument('--n_epochs', action="store", dest="n_epochs", type=int, default=50)
-parser.add_argument('--batch_size', action="store", dest="bs", type=int, default=10)
-parser.add_argument('--learning_rate', action="store", dest="lr", type=float, default=0.01)
+    parser.add_argument('--train_from_scratch', action='store_true', dest='retrain', default=False)
+    parser.add_argument('--use_og_data_only', action='store_true', default=False)
+    parser.add_argument('--use_2_classes', action='store_true', default=False)
+    parser.add_argument('--n_epochs', action="store", type=int, default=50)
+    parser.add_argument('--batch_size', action="store", type=int, default=10)
+    parser.add_argument('--learning_rate', action="store", type=float, default=0.01)
+    parser.add_argument('--hidden_size', action="store", type=float, default=25)
+    parser.add_argument('--num_layers', action="store", type=float, default=-1)
+
+    args = parser.parse_args()
+    return args
 
 
 def build_vocab(posts):
@@ -47,6 +61,16 @@ def strings_to_tensors(posts_array, tok_to_ix):
 
 
 def reorder_minibatch(minibatch_data, minibatch_label):
+    """
+    Helper method that sorts the minibatch data and labels so that the samples are 
+    sorted in order from longest to shortest. This is makes it more efficient to
+    pad different-length samples, according to 
+    https://stackoverflow.com/questions/51030782/why-do-we-pack-the-sequences-in-pytorch
+    
+    (Taken from Dr. Laurent's batching example)
+
+    Returns a tuple of sorted lists: (data, labels)
+    """
     temp = sorted(zip(minibatch_data, minibatch_label),
                   key=lambda x: len(x[0]), reverse=True)
     list1, list2 = map(list, zip(*temp))
@@ -55,7 +79,11 @@ def reorder_minibatch(minibatch_data, minibatch_label):
 
 def make_minibatch(indices, data, label):
     """
-    Extract minibatch from given data and label tensors using the given indices. 
+    Extracts a minibatch from the given data and label tensors using the given indices. 
+    
+    (Taken from Dr. Laurent's batching example)
+
+    Returns a tuple of two tensors: (data, labels)
     """
     minibatch_data = [data[idx.item()] for idx in indices]
     minibatch_label = [label[idx.item()] for idx in indices]
@@ -68,17 +96,101 @@ def make_minibatch(indices, data, label):
     return minibatch_data, minibatch_label
 
 
-def eval_on_test_set(model, use_og_data_only=True, bs=50):
-    """ 
-    Evaluate the model
+def load_data_tensors(use_og_data_only):
     """
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    Load the training data, converts them to tensors, and generates the
+    vocabulary
+    """
+    posts_train, labels_train = load_data(
+        # og_file_path='data/train_reddit_submissions.csv',
+        og_file_path='data/train_reddit_submissions_abridged.csv',
+        aug_file_path='data/train_synonym_augmented_reddit_submissions.csv',
+        include_og=True,
+        include_aug=not use_og_data_only)
+
+    tok_to_ix = build_vocab(posts_train)
+
+    """
+    Convert all posts to tensors that we will input into the model so that we do
+    not have to convert them again at every epoch
+    """
+    data_train = strings_to_tensors(posts_train, tok_to_ix)
+    labels_train = torch.LongTensor(labels_train)
+
+    return data_train, labels_train, tok_to_ix
+
+
+def train_model(
+    model, 
+    loss_func, 
+    optimizer, 
+    data_train, 
+    labels_train, 
+    n_epochs, 
+    batch_size, 
+    learning_rate, 
+    save_path, 
+    device):
+    """
+    Train the given model with the specified hyperparameters for n_epochs 
+    and save the model to a file. 
+    """
+    epochs = trange(1, n_epochs + 1)
+    for epoch in epochs:
+        model.train()
+
+        # Accumulate loss for all samples in this epoch
+        running_loss = 0
+        num_batches = 0
+        num_samples = len(data_train)
+
+        shuffled_indices = torch.randperm(num_samples)
+
+        # Gradient descent algorithm for each data sample
+        batches = trange(0, num_samples - batch_size, batch_size, leave=False)
+        for count in batches:
+            # Extract minibatches and send to GPU
+            indices = shuffled_indices[count: count + batch_size]
+            minibatch_data, minibatch_label = make_minibatch(
+                indices, data_train, labels_train)
+            minibatch_data, minibatch_label = minibatch_data.to(
+                device), minibatch_label.to(device)
+
+            # Make a predictions on the training data
+            scores = model(minibatch_data)
+
+            # Backpropagation
+            loss = loss_func(scores, minibatch_label)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Compute metrics
+            with torch.no_grad():
+                num_batches += 1
+                running_loss += loss.item()
+
+        epochs.set_description(
+            f'Epoch {epoch}/{n_epochs} | Loss: {running_loss/num_batches}')
+
+    torch.save(model, save_path)
+    return model
+
+
+def evaluate_model(model, tok_to_ix, device, use_og_data_only=True, bs=50):
+    """ 
+    Evaluates the given model on the specified test set.
+    """
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     test_data, test_labels = load_data(
         og_file_path='data/test_reddit_submissions.csv',
         aug_file_path='data/test_synonym_augmented_reddit_submissions.csv',
         include_og=True,
         include_aug=not use_og_data_only
     )
+
+    test_data = strings_to_tensors(test_data, tok_to_ix)
+    test_labels = torch.LongTensor(test_labels)
 
     with torch.no_grad():
         model.eval()
